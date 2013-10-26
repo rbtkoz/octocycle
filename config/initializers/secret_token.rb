37059f39827f31397e190ca3605f1f5772cf9c0d
AK <kozovski.a@gmail.com>
2013-10-26 12:11:53 -0400

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Octocycle::Application.config.secret_key_base = 'fe8fe43b156f69c33720ab6dc9de2abeeeb1568ff100f5ccf135f684c5451dbb3fce69fe159fa1a19c3db315460c501e7296fdab78cf3d4a6bd3fe02401f3dfc'
