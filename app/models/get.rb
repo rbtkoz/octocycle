a4404d6d76f045b3567c0001bf8f75cc24fce0c0
AK <kozovski.a@gmail.com>
2013-11-14 01:00:29 -0500

    added s3 info in model

--- app/models/get.rb
+++ app/models/get.rb
@@ -1,6 +1,11 @@
 class Get < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :img, :styles => { :large => "800x800>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/missing.png"
-	
+	has_attached_file :img, :styles => { :large => "800x800>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/missing.png",
+	:storage => :s3,
+    :bucket => 'octocycle',
+    :s3_credentials => {
+      :access_key_id => 'AKIAI3YC6X4GFDGRS5FQ',
+      :secret_access_key => '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'
+    }
 	
 end
